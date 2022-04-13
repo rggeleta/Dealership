9c7ce8468dcff8804f37719fa6a5dee00b7a1f9a
Kai Mwirotsi-Shaw <dickjustice420@gmail.com>
2022-04-13 13:13:21 -0500

    added customer entity

--- Dealership/src/main/java/dealership/beans/Customer.java
+++ Dealership/src/main/java/dealership/beans/Customer.java
@@ -1,10 +1,62 @@
 package dealership.beans;
 
+import javax.persistence.*;
+
 /**
  * @author William Thomas, wdthomas2
  * CIS175 - Spring 2022
  * Apr 11, 2022
  */
-public class Customer {
 
+@Entity
+@Table(name="dealership")
+public class Customer {
+	@Id
+	@GeneratedValue
+	private long id;
+	@Column(name = "FirstName")
+	private String firstName;
+	@Column(name = "LastName")
+	private String lastName;
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public Customer(long id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
 }
