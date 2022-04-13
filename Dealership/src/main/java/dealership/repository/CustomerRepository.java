dc4b3749449a0c9c0bf6fab8fc89e76b230edd24
Henchman117 <90932971+Henchman117@users.noreply.github.com>
2022-04-13 13:30:51 -0500

    Delete Dealership directory

--- Dealership/src/main/java/dealership/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package dealership.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dealership.beans.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
