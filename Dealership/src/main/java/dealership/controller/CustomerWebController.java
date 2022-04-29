4cd900a2ad11e6ecf0b3a13624474df49ef7c1cd
Kai Mwirotsi-Shaw <dickjustice420@gmail.com>
2022-04-29 00:47:33 -0500

    Added selling

--- Dealership/src/main/java/dealership/controller/CustomerWebController.java
+++ Dealership/src/main/java/dealership/controller/CustomerWebController.java
@@ -1,5 +1,7 @@
 package dealership.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +11,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import dealership.repository.CustomerRepository;
+import dealership.repository.VehicleRepository;
 import dealership.beans.Customer;
+import dealership.beans.Vehicle;
 
 @Controller
 public class CustomerWebController {
 	@Autowired
 	CustomerRepository repo;
+	@Autowired
+	VehicleRepository repo_vehicle;
 	
 	@GetMapping("/viewCustomers")
 	public String viewAllCustomers(Model model) {
@@ -42,6 +48,21 @@ public class CustomerWebController {
 	public String deleteCustomer(@PathVariable("id")long id, Model model) {
 		Customer c = repo.findById(id).orElse(null);
 		repo.delete(c);
+		return viewAllCustomers(model);
+	}
+	
+	//Selling vehicles
+	@GetMapping("/sell/{id}")
+	public String sellVehicle(@PathVariable("id") long id, Model model) {
+		Customer c = repo.findById(id).orElse(null);
+		List<Vehicle> v = repo_vehicle.findAll();
+		model.addAttribute("customer", c);
+		model.addAttribute("vehicles", v);
+		return "customerSelect";
+	}
+	@PostMapping("/updateCustomer/{id}")
+	public String updateCustomer(Customer c, Model model) {
+		repo.save(c);
 		return viewAllCustomers(model);
 	}
 }
